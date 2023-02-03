ed46d9ffc4c406abe60dec566ae3e958dc7e4a01
unkwn1 <unkwn1@tutanota.com>
2023-02-03 05:47:40 -0500

    cleaning house

--- tui.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mapfile -t scratchpads < <(python3 /home/unkwn1/gits/scratchpad-waybar-widget/scratchpads.py)
-choice=$(gum choose "${scratchpads[@]}")
-swaymsg "[title=\"${choice}\"] scratchpad show"
